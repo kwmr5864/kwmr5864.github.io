f0bbdd979051239545d7cb15875f999c12f81b05
Masayuki Kawamura <kwmr5864@gmail.com>
2019-01-10 19:13:57 +0900

    add vue-cli3 build project

---
+++ precache-manifest.6e36c58f02e76239a2013b94fc4802cc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "76e974c646674e6e3b42",
+    "url": "/js/chunk-vendors.e9c4afed.js"
+  },
+  {
+    "revision": "f78e80251448b6cbeab8",
+    "url": "/js/app.fe099891.js"
+  },
+  {
+    "revision": "0c544ad7d07a0d5ce03a",
+    "url": "/js/about.d975b428.js"
+  },
+  {
+    "revision": "1bac7e29cabf7ec2057bafe63043afac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "f78e80251448b6cbeab8",
+    "url": "/css/app.0cfee6f7.css"
+  }
+];
